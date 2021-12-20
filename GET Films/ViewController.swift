bdbdb8935a794034cb36b8a63ce0cdc5a1afdb98
Rehab B <79226830+er-otaibi@users.noreply.github.com>
2021-12-20 14:21:08 +0300

    Initial Commit

---
+++ GET Films/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GET Films
+//
+//  Created by Mac on 16/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
